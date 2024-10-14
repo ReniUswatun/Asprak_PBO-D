413dfbd5914d0d7c2e0acc0b36bb3b24a763d731
Rizal Anggoro <gnoogler4@gmail.com>
2024-10-14 14:23:29 +0700

    chore(example code): gatau

--- TodoList/src/main/java/com/todo/Main.java
+++ TodoList/src/main/java/com/todo/Main.java
@@ -1,63 +1,78 @@
 package com.todo;
 
-import com.todo.entities.PriorityTodo;
-import com.todo.entities.Todo;
+// import java.util.Scanner;
 
-import java.util.Scanner;
+// import com.todo.entities.PriorityTodo;
+import com.todo.entities.Todo;
 
 public class Main {
     public static void main(String[] args) {
         // menginsialisasi scanner
-        Scanner scanner = new Scanner(System.in);
+        // Scanner scanner = new Scanner(System.in);
 
-        // meminta user untuk memasukkan beberapa input
-        System.out.println("Masukkan judul");
-        System.out.print("> ");
-        String title = scanner.nextLine();
+        // // meminta user untuk memasukkan beberapa input
+        // System.out.println("Masukkan judul");
+        // System.out.print("> ");
+        // String title = scanner.nextLine();
 
-        System.out.println("Masukkan deskripsi");
-        System.out.print("> ");
-        String description = scanner.nextLine();
+        // System.out.println("Masukkan deskripsi");
+        // System.out.print("> ");
+        // String description = scanner.nextLine();
 
-        // membuat object to-do tanpa priority baru
-        Todo todo = new Todo(title, description);
+        // // membuat object to-do tanpa priority baru
+        // Todo todo = new Todo(title, description);
 
-        // menampilkan to-do tanpa priority ke console
-        todo.display();
+        // // menampilkan to-do tanpa priority ke console
+        // todo.display();
 
-        System.out.println("Mengubah isCompleted menjadi true...");
-        todo.markAsDone();
+        // System.out.println("Mengubah isCompleted menjadi true...");
+        // todo.markAsDone();
 
-        // menampilkan ulang to-do tanpa priority ke console
-        todo.display();
+        // // menampilkan ulang to-do tanpa priority ke console
+        // todo.display();
 
-        // meminta user untuk memasukkan beberapa input
-        System.out.println("Masukkan judul");
-        System.out.print("> ");
-        String title2 = scanner.nextLine();
+        // // meminta user untuk memasukkan beberapa input
+        // System.out.println("Masukkan judul");
+        // System.out.print("> ");
+        // String title2 = scanner.nextLine();
 
-        System.out.println("Masukkan deskripsi");
-        System.out.print("> ");
-        String description2 = scanner.nextLine();
+        // System.out.println("Masukkan deskripsi");
+        // System.out.print("> ");
+        // String description2 = scanner.nextLine();
 
-        System.out.println("Masukkan prioritas");
-        System.out.print("> ");
-        int prioritas2 = scanner.nextInt();
+        // System.out.println("Masukkan prioritas");
+        // System.out.print("> ");
+        // int prioritas2 = scanner.nextInt();
 
-        // membuat object to-do dengan priority baru
-        PriorityTodo priorityTodo = new PriorityTodo(title2, description2, prioritas2);
+        // // membuat object to-do dengan priority baru
+        // PriorityTodo priorityTodo = new PriorityTodo(title2, description2,
+        // prioritas2);
 
-        // menampilkan to-do dengan priority ke console
-        priorityTodo.display();
+        // // menampilkan to-do dengan priority ke console
+        // priorityTodo.display();
 
-        System.out.println("Mengubah isCompleted menjadi true...");
-        priorityTodo.markAsDone();
+        // System.out.println("Mengubah isCompleted menjadi true...");
+        // priorityTodo.markAsDone();
 
-        // menampilkan ulang to-do dengan priority ke console
-        priorityTodo.display();
+        // // menampilkan ulang to-do dengan priority ke console
+        // priorityTodo.display();
 
-        scanner.close();
+        // scanner.close();
 
-        // maaf seadanya, udah trauma dicopas soalnya :)
+        // contoh penerapan enkapsulasi
+        Todo todo2 = new Todo();
+
+        // penerapan setter
+        todo2.setTitle("Ini Judul");
+        todo2.setDescription("Ini description");
+        todo2.setCompleted(true);
+
+        // penerapan getter
+        System.out.println("Judul    : " + todo2.getTitle());
+        System.out.println("Deskripsi: " + todo2.getDescription());
+        System.out.println("Completed: [" + (todo2.isCompleted() ? "v" : " ") + "]");
+
+        // sebenernya ga boleh gini yaa
+        // todo2.display();
     }
 }
--- TodoList/src/main/java/com/todo/entities/Todo.java
+++ TodoList/src/main/java/com/todo/entities/Todo.java
@@ -1,9 +1,9 @@
 package com.todo.entities;
 
 public class Todo {
-    String title;
-    String description;
-    boolean isCompleted;
+    private String title;
+    private String description;
+    private boolean isCompleted;
 
     /**
      * constructor digunakan untuk meng-assign nilai attribute dari object
@@ -11,6 +11,9 @@ public class Todo {
      * constructor pertama hanya membutuhkan parameter title,
      * sedangkan constructor kedua membutuhkan parameter title dan description
      */
+    public Todo() {
+    }
+
     public Todo(String title) {
         this.title = title;
     }
@@ -35,5 +38,29 @@ public class Todo {
 
     public void markAsDone() {
         this.isCompleted = true;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCompleted(boolean isCompleted) {
+        this.isCompleted = isCompleted;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
     }
 }
