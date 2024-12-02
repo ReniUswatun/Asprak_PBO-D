3a1e43bd2e282ca827c49607ce13d383ef529dd3
Rizal Anggoro <gnoogler4@gmail.com>
2024-12-02 17:14:49 +0700

    feat: add OkHttp dependency and implement basic HTTP request in Main class

--- TodoList/src/main/java/com/todo/Main.java
+++ TodoList/src/main/java/com/todo/Main.java
@@ -1,78 +1,30 @@
 package com.todo;
 
-// import java.util.Scanner;
-
-// import com.todo.entities.PriorityTodo;
-import com.todo.entities.Todo;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class Main {
-    public static void main(String[] args) {
-        // menginsialisasi scanner
-        // Scanner scanner = new Scanner(System.in);
+    public static void main(String args[]) {
+        Thread myThread = new Thread(() -> {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url("https://jsonplaceholder.typicode.com/photos")
+                    .build();
 
-        // // meminta user untuk memasukkan beberapa input
-        // System.out.println("Masukkan judul");
-        // System.out.print("> ");
-        // String title = scanner.nextLine();
+            try {
+                Response response = client.newCall(request).execute();
+                if (response.code() == 200) {
+                    System.out.println("success");
+                }
+            } catch (Exception e) {
+                System.err.println(e);
+            }
+        });
 
-        // System.out.println("Masukkan deskripsi");
-        // System.out.print("> ");
-        // String description = scanner.nextLine();
+        System.out.println("sebelum thread");
+        myThread.start();
+        System.out.println("setelah thread");
 
-        // // membuat object to-do tanpa priority baru
-        // Todo todo = new Todo(title, description);
-
-        // // menampilkan to-do tanpa priority ke console
-        // todo.display();
-
-        // System.out.println("Mengubah isCompleted menjadi true...");
-        // todo.markAsDone();
-
-        // // menampilkan ulang to-do tanpa priority ke console
-        // todo.display();
-
-        // // meminta user untuk memasukkan beberapa input
-        // System.out.println("Masukkan judul");
-        // System.out.print("> ");
-        // String title2 = scanner.nextLine();
-
-        // System.out.println("Masukkan deskripsi");
-        // System.out.print("> ");
-        // String description2 = scanner.nextLine();
-
-        // System.out.println("Masukkan prioritas");
-        // System.out.print("> ");
-        // int prioritas2 = scanner.nextInt();
-
-        // // membuat object to-do dengan priority baru
-        // PriorityTodo priorityTodo = new PriorityTodo(title2, description2,
-        // prioritas2);
-
-        // // menampilkan to-do dengan priority ke console
-        // priorityTodo.display();
-
-        // System.out.println("Mengubah isCompleted menjadi true...");
-        // priorityTodo.markAsDone();
-
-        // // menampilkan ulang to-do dengan priority ke console
-        // priorityTodo.display();
-
-        // scanner.close();
-
-        // contoh penerapan enkapsulasi
-        Todo todo2 = new Todo();
-
-        // penerapan setter
-        todo2.setTitle("Ini Judul");
-        todo2.setDescription("Ini description");
-        todo2.setCompleted(true);
-
-        // penerapan getter
-        System.out.println("Judul    : " + todo2.getTitle());
-        System.out.println("Deskripsi: " + todo2.getDescription());
-        System.out.println("Completed: [" + (todo2.isCompleted() ? "v" : " ") + "]");
-
-        // sebenernya ga boleh gini yaa
-        // todo2.display();
     }
 }
